93bc5063cd492cfb416d39ae3ccaa48951277933
David Robertsson <david.robertsson@gmail.com>
2023-01-23 23:19:36 +0100

    formatting.

--- config/runtime.exs
+++ config/runtime.exs
@@ -54,7 +54,6 @@ if config_env() == :prod do
   config :coop_slide,
     uploads_directory: System.get_env("SLIDE_UPLOADS_DIRECTORY") || "/uploads"
 
-
   config :coop_slide, CoopSlideWeb.Endpoint,
     url: [host: host, port: 443, scheme: "https"],
     check_origin: [
